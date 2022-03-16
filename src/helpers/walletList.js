91ed8d5e41995b1c8860a8074d52411060cde58d
Chris Cassano <1285652+glitch003@users.noreply.github.com>
2022-03-16 19:11:06 -0400

    replace reference to window with globalThis

--- src/helpers/walletList.js
+++ src/helpers/walletList.js
@@ -1,29 +1,30 @@
-import metaMaskLogo from '../logos/metamask.svg';
-import walletConnectLogo from '../logos/walletconnect.svg';
+import metaMaskLogo from "../logos/metamask.svg";
+import walletConnectLogo from "../logos/walletconnect.svg";
 
 const rawListOfWalletsArray = [
   {
-    htmlId: 'lcm-metaMask',
-    id: 'metamask',
+    htmlId: "lcm-metaMask",
+    id: "metamask",
     logo: metaMaskLogo,
-    name: 'MetaMask',
-    provider: window.ethereum,
-    synopsis: 'Connect your MetaMask Wallet',
+    name: "MetaMask",
+    provider: globalThis.ethereum,
+    synopsis: "Connect your MetaMask Wallet",
     checkIfPresent: () => {
-      if (typeof window.ethereum !== 'undefined') {
+      if (typeof globalThis.ethereum !== "undefined") {
         return true;
       } else {
         return false;
       }
-    }
-  }, {
-    htmlId: 'lcm-walletConnect',
-    id: 'walletconnect',
+    },
+  },
+  {
+    htmlId: "lcm-walletConnect",
+    id: "walletconnect",
     logo: walletConnectLogo,
-    name: 'Wallet Connect',
+    name: "Wallet Connect",
     provider: null,
-    synopsis: 'Scan with WalletConnect to connect',
-  }
-]
+    synopsis: "Scan with WalletConnect to connect",
+  },
+];
 
 export default rawListOfWalletsArray;
